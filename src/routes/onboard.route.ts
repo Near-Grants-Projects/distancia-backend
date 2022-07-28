f87ad832a071658acd750e13b99cf987e7fc8437
marvikomo <marvikomo@gmail.com>
2022-07-28 08:54:36 +0100

    update

--- src/routes/onboard.route.ts
+++ src/routes/onboard.route.ts
@@ -4,6 +4,6 @@ import  OnboardController from "../controllers/onboard.controller"
 const onbaordController: any = container.resolve(OnboardController)
 
 const router = express.Router();
-router.post("/email-otp", onbaordController.doSomething);
+router.post("/me", onbaordController.doSomething);
 
 export default router;

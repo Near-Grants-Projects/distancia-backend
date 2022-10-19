94e732ad95ff8640c9a2c29605fda6c86f9511eb
marvikomo <marvikomo@gmail.com>
2022-10-20 00:05:04 +0100

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ export default {
   // MONGODB_URL: env.MONGODB_URL || 'mongodb://127.0.0.1:27017/distancia',
   MONGODB_URL:
     env.MONGODB_URL ||
-    'mongodb+srv://sa:user12345678@citisquarecluster.bfvncum.mongodb.net/dist',
+    'mongodb+srv://marvex:ikponmwosa@cluster0.5u2yi.mongodb.net/distancia?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'ss4.@SGGio082',
   HEADER_NAME: process.env.HEADER_NAME || 'distancia-auth-token',
   JWT_DURATION: process.env.JWT_DURATION || '1h',
